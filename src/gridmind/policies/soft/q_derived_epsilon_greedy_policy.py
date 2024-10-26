ebcaea81db48e8f60f4774cc0434ca5cd69e1d65
shuvoxcd01 <dasshuvo7@gmail.com>
2024-10-26 12:19:27 +0600

    Add epsilon decay

--- src/gridmind/policies/soft/q_derived_epsilon_greedy_policy.py
+++ src/gridmind/policies/soft/q_derived_epsilon_greedy_policy.py
@@ -3,24 +3,26 @@ from typing import Dict, Optional
 from gymnasium import Space
 import numpy as np
 
-from gridmind.policies.base_q_derived_policy import BaseQDerivedPolicy
-from gridmind.policies.base_soft_policy import BaseSoftPolicy
+from gridmind.policies.soft.base_q_derived_soft_policy import BaseQDerivedSoftPolicy
 
 
-
-class QDerivedEpsilonGreedyPolicy(BaseQDerivedPolicy, BaseSoftPolicy):
+class QDerivedEpsilonGreedyPolicy(BaseQDerivedSoftPolicy):
     def __init__(
         self,
         q_table: Dict,
         num_actions: int,
         action_space: Optional[Space] = None,
         epsilon: float = 0.1,
+        allow_decay: bool = True,
+        epsilon_min: float = 0.001,
+        decay_rate: float = 0.01,
     ) -> None:
-        BaseQDerivedPolicy.__init__(self, q_table=q_table)
-        BaseSoftPolicy.__init__(self)
+        super().__init__(q_table=q_table, epsilon=epsilon)
         self.num_actions = num_actions
         self.action_space = action_space
-        self.epsilon = epsilon
+        self.allow_decay = allow_decay
+        self.epsilon_min = epsilon_min
+        self.decay_rate = decay_rate
 
         assert epsilon >= 0 and epsilon <= 1, "epsilon must be in rage 0 to 1."
         assert (
@@ -69,3 +71,22 @@ class QDerivedEpsilonGreedyPolicy(BaseQDerivedPolicy, BaseSoftPolicy):
     def get_action_deterministic(self, state):
         action = self._get_greedy_action(state=state)
         return action
+
+    def set_epsilon(self, value: float):
+        if value < self.epsilon_min:
+            self.logger.warning(
+                f"Trying to set epsilon value less than epsilon_min. Setting epsilon=epsilon_min"
+            )
+            value = self.epsilon_min
+
+        super().set_epsilon(value)
+
+    def decay_epsilon(self):
+        if not self.allow_decay:
+            self.logger.warning("Epsilon decay is not allowed.")
+            return
+        
+        decayed_epsilon = self.epsilon - self.decay_rate
+
+        if decayed_epsilon >= self.epsilon_min:
+            self.set_epsilon(value=decayed_epsilon)
