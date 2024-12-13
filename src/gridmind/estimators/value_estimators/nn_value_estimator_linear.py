2aa2c838682aaf24d84eb661e0440d3fd0aad985
shuvoxcd01 <dasshuvo7@gmail.com>
2024-12-13 12:26:54 +0600

    Add option to selectively use bias

--- src/gridmind/estimators/value_estimators/nn_value_estimator_linear.py
+++ src/gridmind/estimators/value_estimators/nn_value_estimator_linear.py
@@ -2,5 +2,5 @@ from gridmind.estimators.value_estimators.nn_value_estimator import NNValueEstim
 
 
 class NNValueEstimatorLinear(NNValueEstimator):
-    def __init__(self, observation_shape: tuple):
-        super().__init__(observation_shape, num_hidden_layers=0)
+    def __init__(self, observation_shape: tuple, use_bias: bool = False):
+        super().__init__(observation_shape, num_hidden_layers=0, use_bias=use_bias)
