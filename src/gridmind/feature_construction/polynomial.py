0acc6a957b7d582c4904059f97ba614e41ee20ac
shuvoxcd01 <dasshuvo7@gmail.com>
2024-12-15 13:38:40 +0600

    Update polynomial feature construction

--- src/gridmind/feature_construction/polynomial.py
+++ src/gridmind/feature_construction/polynomial.py
@@ -1,4 +1,6 @@
-from typing import Optional
+import itertools
+import numbers
+from typing import Union
 
 import numpy as np
 
@@ -7,34 +9,27 @@ class PolynomialFeatureConstructor:
     def __init__(
         self,
         n: int,
-        k: int,
-        num_features: Optional[int] = None,
-        seed: int = 42,
     ):
         """
         n: Maximum Degree of Each Component (output) in the Polynomial Basis
-        k: Dimensionality of the (input) State Space (number of components in the input)
         """
-        assert n >= 2 and k >= 1
 
-        self.num_features = num_features if num_features is not None else (n + 1) ** k
+        C = np.arange(0, n + 1)
+        exponents = lambda k: np.array(list(itertools.product(C, repeat=k)))
 
-        assert self.num_features <= (n + 1) ** k
+        self._feature_constuctor = lambda s: np.prod(np.power(s, exponents(s.size)), axis=1)
 
-        rng = np.random.default_rng(seed=seed)
+    def __call__(self, state: Union[np.ndarray, numbers.Number], *args, **kwds):
+        if isinstance(state, numbers.Number):
+            state = np.array(state)
 
-        exponents = rng.integers(0, n + 1, size=(self.num_features, k))
-
-        self._feature_constuctor = lambda s: np.prod(np.power(s, exponents), axis=1)
-
-    def __call__(self, state: np.ndarray, *args, **kwds):
         features = self._feature_constuctor(state)
         return features
 
 
 if __name__ == "__main__":
-    state = np.array([1, 0, -1])
-    fc = PolynomialFeatureConstructor(n=3, k=3)
+    state = np.array([2,2,3])
+    fc = PolynomialFeatureConstructor(n=1)
     features_1 = fc(state)
     features_2 = fc(state)
 
