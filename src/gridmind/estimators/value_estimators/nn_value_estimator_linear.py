41df06271ca14fd327698267a19a9dc3b8293bfa
shuvoxcd01 <dasshuvo7@gmail.com>
2024-12-24 22:28:33 +0600

    Rename estimators

--- src/gridmind/estimators/value_estimators/nn_value_estimator_linear.py
+++
@@ -1,6 +0,0 @@
-from gridmind.estimators.value_estimators.nn_value_estimator import NNValueEstimator
-
-
-class NNValueEstimatorLinear(NNValueEstimator):
-    def __init__(self, observation_shape: tuple, use_bias: bool = False):
-        super().__init__(observation_shape, num_hidden_layers=0, use_bias=use_bias)
