1c5ed74e11aa53db4d26100ec0b4002509133972
shuvoxcd01 <dasshuvo7@gmail.com>
2024-12-24 23:13:27 +0600

    Parameterize in and out feature num

--- src/gridmind/estimators/base_nn_estimator.py
+++ src/gridmind/estimators/base_nn_estimator.py
@@ -8,23 +8,29 @@ class BaseNNEstimator(nn.Module):
         observation_shape: tuple,
         num_hidden_layers: int = 0,
         num_outputs: int = 1,
+        in_features: int = 16,
+        out_features: int = 16,
         use_bias: bool = True,
     ):
         super().__init__()
-        num_in_features = math.prod(observation_shape)
+        num_input_features = math.prod(observation_shape)
         self.num_hidden_layers = num_hidden_layers
+        self.in_features = in_features
+        self.out_features = out_features
         self.hidden_layers = nn.ModuleList()
 
         if self.num_hidden_layers <= 0:
             self.linear_out = nn.Linear(
-                in_features=num_in_features, out_features=num_outputs, bias=use_bias
+                in_features=num_input_features, out_features=num_outputs, bias=use_bias
             )
 
         else:
             self.hidden_layers.append(
                 nn.Sequential(
                     nn.Linear(
-                        in_features=num_in_features, out_features=256, bias=use_bias
+                        in_features=num_input_features,
+                        out_features=self.out_features,
+                        bias=use_bias,
                     ),
                     nn.ReLU(),
                 )
@@ -33,10 +39,14 @@ class BaseNNEstimator(nn.Module):
             for _ in range(self.num_hidden_layers - 1):
                 self.hidden_layers.append(self._create_hidden_layer(use_bias=use_bias))
 
-            self.linear_out = nn.Linear(in_features=256, out_features=num_outputs, bias=use_bias)
+            self.linear_out = nn.Linear(
+                in_features=self.in_features, out_features=num_outputs, bias=use_bias
+            )
 
     def _create_hidden_layer(self, use_bias: bool):
-        return nn.Sequential(nn.Linear(256, 256, bias=use_bias), nn.ReLU())
+        return nn.Sequential(
+            nn.Linear(self.in_features, self.out_features, bias=use_bias), nn.ReLU()
+        )
 
     def forward(self, x):
         for hidden_layer in self.hidden_layers:
