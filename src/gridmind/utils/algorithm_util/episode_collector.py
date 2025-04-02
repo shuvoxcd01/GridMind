7b4a9c67dc1b9ab0a783d8a070c2432ac2021d55
shuvoxcd01 <dasshuvo7@gmail.com>
2025-04-02 22:54:18 +0600

    Add optional preprocessor

--- src/gridmind/utils/algorithm_util/episode_collector.py
+++ src/gridmind/utils/algorithm_util/episode_collector.py
@@ -1,15 +1,18 @@
 
+from typing import Callable, Optional
 from gridmind.policies.base_policy import BasePolicy
 from gridmind.utils.algorithm_util.trajectory import Trajectory
 from gymnasium import Env
 
 
-def collect_episode(env:Env, policy:BasePolicy, trajectory:Trajectory):
+def collect_episode(env:Env, policy:BasePolicy, trajectory:Trajectory, obs_preprocessor:Optional[Callable]=None):
     trajectory.clear()
     obs, info = env.reset()
     done = False
 
     while not done:
+        if obs_preprocessor is not None:
+            obs = obs_preprocessor(obs)
         action = policy.get_action(state=obs)
         next_obs, reward, terminated, truncated, info = env.step(action)
         trajectory.record_step(state=obs, action=action, reward=reward)
