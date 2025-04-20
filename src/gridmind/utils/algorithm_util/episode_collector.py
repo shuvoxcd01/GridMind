3f018ef4119a2bcf9250e0a80c4da16b19eacfc5
shuvoxcd01 <dasshuvo7@gmail.com>
2025-04-20 15:06:13 +0600

    Update episode collector

--- src/gridmind/utils/algorithm_util/episode_collector.py
+++ src/gridmind/utils/algorithm_util/episode_collector.py
@@ -1,20 +1,20 @@
 
 from typing import Callable, Optional
-from typing import Callable, Optional
 from gridmind.policies.base_policy import BasePolicy
 from gridmind.utils.algorithm_util.trajectory import Trajectory
 from gymnasium import Env
 
 
-def collect_episode(env:Env, policy:BasePolicy, trajectory:Trajectory, obs_preprocessor:Optional[Callable]=None):
+def collect_episode(env:Env, policy:BasePolicy, trajectory:Trajectory, obs_preprocessor:Optional[Callable]=None, record_action_prob:bool = False):
     trajectory.clear()
     obs, info = env.reset()
     done = False
 
     while not done:
+        obs_raw = obs
         if obs_preprocessor is not None:
             obs = obs_preprocessor(obs)
-        action = policy.get_action(state=obs)
+        action = policy.get_action(state=obs)            
         next_obs, reward, terminated, truncated, info = env.step(action)
         
         if record_action_prob:
