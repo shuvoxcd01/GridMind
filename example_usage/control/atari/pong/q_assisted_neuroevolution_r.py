9e315ab2ab155199447ab80fc0808b1792c97bbe
Falguni Das Shuvo <dasshuvo7@gmail.com>
2025-05-11 21:07:01 +0600

    Update example

--- example_usage/control/atari/pong/q_assisted_neuroevolution_r.py
+++ example_usage/control/atari/pong/q_assisted_neuroevolution_r.py
@@ -1,14 +1,23 @@
-from gridmind.algorithms.evolutionary_rl.neuroevolution.value_fn_assisted_neuroevolution__with_state_dict_r import QAssistedNeuroEvolution
+from gridmind.algorithms.evolutionary_rl.neuroevolution.value_fn_assisted_neuroevolution_r import QAssistedNeuroEvolution
 from gridmind.algorithms.function_approximation.temporal_difference.control.deep_q_learning_experience_r import DeepQLearningWithExperienceReplay
 from gridmind.policies.parameterized.atari_policy import AtariPolicy
 from gridmind.value_estimators.action_value_estimators.atari_deep_q_estimator import AtariDQN
 import gymnasium as gym
+from gymnasium.wrappers import AtariPreprocessing, FrameStackObservation
 import ale_py
 
 gym.register_envs(ale_py)
-env = gym.make('ALE/Breakout-v5')
+env = gym.make('ALE/Breakout-v5', frameskip=1)
+env = AtariPreprocessing(
+            env,
+            noop_max=10, frame_skip=4, terminal_on_life_loss=True,
+             screen_size=84, grayscale_obs=True, grayscale_newaxis=False
+         )
+env = FrameStackObservation(env, 4)
 q_network = AtariDQN(observation_shape=env.observation_space.shape, num_actions=env.action_space.n)
-algorithm = QAssistedNeuroEvolution(env=env, policy_class=AtariPolicy, write_summary=True, q_network=q_network, mu=50, _lambda=250)
+algorithm = QAssistedNeuroEvolution(env=env, policy_class=AtariPolicy, write_summary=True, 
+                                    q_network=q_network, mu=50, _lambda=250, replay_buffer_capacity=10000, 
+                                    score_evaluation_num_episodes=1, k=10, reevaluate_agent_score=False)
 
 try:
     best_agent = algorithm.train(
@@ -17,7 +26,14 @@ try:
 except KeyboardInterrupt:
     print("Training interrupted.")
 
-eval_env = gym.make('ALE/Breakout-v5', render_mode="human")
+eval_env = gym.make('ALE/Breakout-v5', render_mode="human", frameskip=1)
+eval_env = AtariPreprocessing(
+            eval_env,
+            noop_max=10, frame_skip=4, terminal_on_life_loss=True,
+             screen_size=84, grayscale_obs=True, grayscale_newaxis=False
+         )
+eval_env = FrameStackObservation(eval_env, 4)
+
 policy = best_agent.network
 
 obs, info = eval_env.reset()
