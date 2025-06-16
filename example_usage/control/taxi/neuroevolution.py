834314c7c8c3ed0205e7af2005d1cd7cdf97ed80
shuvoxcd01 <dasshuvo7@gmail.com>
2025-06-16 11:03:51 +0600

    Add examples

---
+++ example_usage/control/taxi/neuroevolution.py
@@ -0,0 +1,42 @@
+from gridmind.algorithms.evolutionary_rl.neuroevolution.neuroevolution import NeuroEvolution
+from gridmind.algorithms.evolutionary_rl.neuroevolution.value_fn_assisted_neuroevolution_r import QAssistedNeuroEvolution
+from gridmind.feature_construction.multi_hot import MultiHotEncoder
+from gridmind.feature_construction.tile_coding import TileCoding
+import gymnasium as gym
+
+
+env = gym.make("Taxi-v3")
+# num_tilings = 7
+# multi_hot_encoder = MultiHotEncoder(num_categories=num_tilings**4)
+# tile_encoder = TileCoding(ihtORsize=num_tilings**4, numtilings=num_tilings)
+# feature_constructor = lambda x: multi_hot_encoder(tile_encoder(x))
+feature_constructor = None
+
+algorithm = NeuroEvolution(env=env, write_summary=True, feature_constructor=feature_constructor)
+
+try:
+    best_agent = algorithm.train(
+        num_generations=10000
+    )
+except Exception as e:
+    print(f"Training interrupted: {e}")
+    best_agent = algorithm.get_best(unwrapped=False)
+
+eval_env = gym.make("Taxi-v3", render_mode="human")
+
+policy = best_agent.network
+
+obs, info = eval_env.reset()
+done = False
+
+episode_return = 0.0
+
+while not done:
+    eval_env.render()
+    obs = algorithm._preprocess(obs)
+    action = policy.get_action(obs)
+    obs, reward, terminated, truncated, info = eval_env.step(action)
+    episode_return += reward
+    done = terminated or truncated
+
+print(f"Episode return: {episode_return}")
---
+++ example_usage/control/taxi/q_assisted_neuroevolution_r.py
@@ -0,0 +1,52 @@
+from gridmind.algorithms.evolutionary_rl.neuroevolution.value_fn_assisted_neuroevolution_r import QAssistedNeuroEvolution
+from gridmind.feature_construction.embedding_feature_extractor import EmbeddingFeatureExtractor
+from gridmind.feature_construction.one_hot import OneHotEncoder
+from gridmind.policies.parameterized.discrete_action_mlp_policy import DiscreteActionMLPPolicy
+from gridmind.value_estimators.action_value_estimators.q_network_with_embedding import QNetworkWithEmbedding
+import gymnasium as gym
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
+env = gym.make("Taxi-v3")
+# one_hot_encoder = OneHotEncoder(num_classes=500)
+q_network = QNetworkWithEmbedding(num_embeddings=500, embedding_dim=32,num_hidden_layers=2, num_actions=env.action_space.n)
+embedding_layer = q_network.get_embedding()
+embedding_feature_constructor = EmbeddingFeatureExtractor(embedding=embedding_layer)
+feature_constructor = lambda x: embedding_feature_constructor(x)
+
+
+policy_creator_fn = lambda observation_shape, num_actions: DiscreteActionMLPPolicy(
+    observation_shape=observation_shape,
+    num_actions=num_actions,
+    num_hidden_layers=2, 
+)
+algorithm = QAssistedNeuroEvolution(env=env,policy_network_creator_fn=policy_creator_fn, write_summary=True, feature_constructor=feature_constructor, q_learner_num_steps=1000, q_learner_target_network_update_frequency=500, replay_buffer_minimum_size=1000, replay_buffer_capacity=5000)
+
+
+try:
+    best_agent = algorithm.train(
+        num_generations=10000
+    )
+except KeyboardInterrupt as e:
+    print(f"Training interrupted: {e}")
+    best_agent = algorithm.get_best(unwrapped=False)
+
+eval_env = gym.make("Taxi-v3", render_mode="human")
+
+policy = best_agent.network
+
+obs, info = eval_env.reset()
+done = False
+
+episode_return = 0.0
+
+while not done:
+    eval_env.render()
+    obs = algorithm._preprocess(obs)
+    action = policy.get_action(obs)
+    obs, reward, terminated, truncated, info = eval_env.step(action)
+    episode_return += reward
+    done = terminated or truncated
+
+print(f"Episode return: {episode_return}")
