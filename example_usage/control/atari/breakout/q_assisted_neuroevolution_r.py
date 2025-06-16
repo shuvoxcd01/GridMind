d2620e9058e8740cb80123187dafc0a6df7afcf5
shuvoxcd01 <dasshuvo7@gmail.com>
2025-06-16 10:55:46 +0600

    Rename parameter

--- example_usage/control/atari/breakout/q_assisted_neuroevolution_r.py
+++ example_usage/control/atari/breakout/q_assisted_neuroevolution_r.py
@@ -18,7 +18,7 @@ env = AtariPreprocessing(
          )
 env = FrameStackObservation(env, 4)
 q_network = AtariDQN(observation_shape=env.observation_space.shape, num_actions=env.action_space.n)
-algorithm = QAssistedNeuroEvolution(env=env, policy_class=AtariPolicy, write_summary=True, 
+algorithm = QAssistedNeuroEvolution(env=env, policy_network_class=AtariPolicy, write_summary=True, 
                                     q_network=q_network, mu=50, _lambda=250, replay_buffer_capacity=10000, 
                                     score_evaluation_num_episodes=1, num_top_k=10, reevaluate_agent_score=False)
 
