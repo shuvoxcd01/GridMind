022367e1e88f1783f89f7b2d4ba5243334a6a00e
Falguni Das Shuvo <dasshuvo7@gmail.com>
2025-06-30 12:41:04 +0600

    Refactor configuration loading and environment setup in Q-assisted neuroevolution script

--- example_usage/control/taxi/q_assisted_neuroevolution_r.py
+++ example_usage/control/taxi/q_assisted_neuroevolution_r.py
@@ -14,36 +14,67 @@ from gymnasium.wrappers import RecordVideo
 from datetime import datetime
 
 from data import SAVE_DATA_DIR
+from gridmind.algorithms.evolutionary_rl.neuroevolution.config_loader import ConfigLoader
+from gridmind.utils.evo_util.selection import Selection
 
 logging.basicConfig(level=logging.INFO)
 
-env = gym.make("Taxi-v3")
+
+
+config_dir = "example_usage/control/taxi/configs"
+config_loader = ConfigLoader()
+config_file_path = os.path.join(config_dir, "basic_config.ini")
+config = config_loader.load_config(config_file_path)
+env_name = config.pop("env_name", "Taxi-v3")
+if env_name == "Taxi-v3":
+    env = gym.make("Taxi-v3")
+else:
+    raise ValueError(f"Unsupported environment: {env_name}. Expected 'Taxi-v3'.")
+
 
 # one_hot_encoder = OneHotEncoder(num_classes=500)
-# q_network = QNetworkWithEmbedding(num_embeddings=500, embedding_dim=64,num_hidden_layers=1, num_actions=env.action_space.n)
-q_network = torch.load("/Users/falguni/Study/Repositories/GitHub/GridMind/data/Taxi-v3/DeepQLearning/2025-06-22_22-03-57/q_network.pth")
-q_learner = DeepQLearningWithExperienceReplay(env = env, q_network=q_network, batch_size=512, 
-                                              feature_constructor=None, write_summary=True, 
-                                              target_network_update_frequency=500)
-embedding_layer = q_network.get_embedding()
-embedding_feature_constructor = EmbeddingFeatureExtractor(embedding=embedding_layer)
-feature_constructor = lambda x: embedding_feature_constructor(x)
+q_network = config.pop("q_network", None)
+if q_network is None:
+    q_network = QNetworkWithEmbedding(num_embeddings=500, embedding_dim=64,num_hidden_layers=1, num_actions=env.action_space.n)
+# q_network = torch.load("/Users/falguni/Study/Repositories/GitHub/GridMind/data/Taxi-v3/DeepQLearning/2025-06-22_22-03-57/q_network.pth")
+
+q_learner = config.pop("q_learner", None)
+if q_learner is None:
+    q_learner = DeepQLearningWithExperienceReplay(env = env, q_network=q_network, batch_size=32, 
+                                                feature_constructor=None, write_summary=True)
+
+feature_constructor = config.pop("feature_constructor", None)
+if feature_constructor is None:
+    embedding_layer = q_network.get_embedding()
+    embedding_feature_constructor = EmbeddingFeatureExtractor(embedding=embedding_layer)
+    feature_constructor = lambda x: embedding_feature_constructor(x)
+
+policy_network_creator_fn = config.pop("policy_network_creator_fn", None)
+if policy_network_creator_fn is None:
+    policy_network_creator_fn = lambda observation_shape, num_actions: DiscreteActionMLPPolicy(
+        observation_shape=observation_shape,
+        in_features=64,
+        out_features=64,
+        num_actions=num_actions,
+        num_hidden_layers=4, 
+    )
+
+num_generations_to_run = config.pop("num_generations_to_run", 100)
+
+algorithm = QAssistedNeuroEvolution(env=env,
+                                    policy_network_creator_fn=policy_network_creator_fn,
+                                    feature_constructor=feature_constructor,
+                                    q_learner=q_learner,
+                                    **config
+                                    )
 
 
-policy_creator_fn = lambda observation_shape, num_actions: DiscreteActionMLPPolicy(
-    observation_shape=observation_shape,
-    in_features=64,
-    out_features=64,
-    num_actions=num_actions,
-    num_hidden_layers=4, 
-)
-algorithm = QAssistedNeuroEvolution(env=env,policy_network_creator_fn=policy_creator_fn,
-                                    write_summary=True, feature_constructor=feature_constructor,
-                                    replay_buffer_capacity=5000, q_learner=q_learner, mutation_std=0.1, mutation_std_max=0.5,mutation_std_min=0.01,
-                                    evaluate_q_derived_policy=False, q_learner_num_steps=1000,
-                                    train_q_learner=False, stagnation_patience=1000)
-
-
+#ToDos:
+# 1. evaluate the q_derived policy
+# 2. Maybe increase the number of elites
+# 3. Use random selection or use elites for training the q_learner (See what works best)
+# 4. May be use full q learning instead of experience replay
+# 5. Curate elite observations and actions for training/evaluating the q_learner
 
 eval_env = gym.make("Taxi-v3", render_mode="rgb_array")
 video_dir = os.path.join(SAVE_DATA_DIR, eval_env.spec.id, algorithm.name, "videos", f"-{datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}")
@@ -52,15 +83,15 @@ os.makedirs(video_dir, exist_ok=True)
 eval_env = RecordVideo(
     eval_env,
     video_folder=video_dir,
-    episode_trigger=lambda episode_id: episode_id % 2 == 0  # record every 2nd episode
+    episode_trigger=lambda episode_id: episode_id % 5 == 0
 )
 
-evaluator = BasicPerformanceEvaluator(env=eval_env, num_episodes=5, epoch_eval_interval= 10)
+evaluator = BasicPerformanceEvaluator(env=eval_env, num_episodes=5, epoch_eval_interval= 25)
 algorithm.register_performance_evaluator(evaluator)
 
 try:
     best_agent = algorithm.train(
-        num_generations=10000
+        num_generations= num_generations_to_run,
     )
 except KeyboardInterrupt as e:
     print(f"Training interrupted: {e}")
