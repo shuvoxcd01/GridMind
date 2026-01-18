68339a3f75d37ad652e4a42855db3d104cff038e
Falguni Das Shuvo <dasshuvo7@gmail.com>
2026-01-18 11:38:25 +0600

    fix: correct training function reference in BaseEvoRLAlgorithm

--- src/gridmind/algorithms/evolutionary_rl/base_evo_rl_algorithm.py
+++ src/gridmind/algorithms/evolutionary_rl/base_evo_rl_algorithm.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 from typing import Optional
 from gridmind.algorithms.base_learning_algorithm import BaseLearningAlgorithm
 from gridmind.policies.base_policy import BasePolicy
@@ -34,10 +35,16 @@ class BaseEvoRLAlgorithm(BaseLearningAlgorithm):
     def _train_steps(self, num_steps: int, prediction_only: bool, *args, **kwargs):
         raise NotImplementedError
 
+    @abstractmethod
+    def _train(self, num_generations: int, *args, **kwargs):
+        raise NotImplementedError(
+            "This method should be implemented in the derived class."
+        )
+
     def train(self, num_generations: int, save_policy: bool = True):
         self._training_wrapper(
             num_iter=num_generations,
             prediction_only=False,
             save_policy=save_policy,
-            training_fn=self.train,
+            training_fn=self._train,
         )
